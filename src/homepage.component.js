f40d2cdbd4d41ce26a2dcc86c875c21e79f84138
Boi Soth <cambodianjesuss@gmail.com>
2021-12-29 21:41:17 -0800

    edits

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -22,13 +22,13 @@ const Homepage = () => (
       </div>
       <div className='menu-item'>
         <div className='content'>
-          <h1 className='title'>WOMEN</h1>
+          <h1 className='title'>WOMENS</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
       <div className='menu-item'>
         <div className='content'>
-          <h1 className='title'>MEN</h1>
+          <h1 className='title'>MENS</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
