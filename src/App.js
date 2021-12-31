a74170f83307e853795de62a7f68140412a1ff94
Boi Soth <cambodianjesuss@gmail.com>
2021-12-31 00:22:13 -0800

    router new version requires some change as component isn't working and will pass a null -- needed yo use element prop  and pass in an element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route exact path="/" element={<Homepage/>} />
+      </Routes>
     </div>
   );
 }
