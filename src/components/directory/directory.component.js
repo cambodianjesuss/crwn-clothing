18b7a6bcdfc4e8aa944aa86ea01c843b40aecf5c
Boi Soth <cambodianjesuss@gmail.com>
2021-12-31 02:28:55 -0800

    using spread operator to grab all props with it's values

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -12,7 +12,7 @@ class Directory extends Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'shop/hats',
         },
         {
           title: 'jackets',
@@ -47,9 +47,9 @@ class Directory extends Component {
     return(
       <div className='directory-menu'>
         {
-          this.state.sections.map(({ title, imageUrl, id, size}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-          ))
+          this.state.sections.map(({ id, ...otherSectionProps}) => (
+            <MenuItem key={id} {...otherSectionProps}/>
+          )) 
         }
       </div>
     )
