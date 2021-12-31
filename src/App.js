55b691fc88efb0b68aaf579813ba1671fc7116b2
Boi Soth <cambodianjesuss@gmail.com>
2021-12-31 14:51:41 -0800

    cleaning up page from testing Hats

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
-
 function App() {
   return (
     <div>
