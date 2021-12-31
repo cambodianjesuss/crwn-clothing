0c6d6d7faba75b2b601e5f5991ec59dd9d2fb086
Boi Soth <cambodianjesuss@gmail.com>
2021-12-31 00:34:33 -0800

    Routes replaced Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+//https://reactrouter.com/docs/en/v6/upgrading/v5
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
@@ -14,6 +15,7 @@ function App() {
     <div>
       <Routes>
         <Route exact path="/" element={<Homepage/>} />
+        <Route path="/hats" element={<HatsPage/>}/>
       </Routes>
     </div>
   );
