712abeade6d13c6502d3316dbf45f88813159336
Boi Soth <cambodianjesuss@gmail.com>
2021-12-31 16:02:09 -0800

    cleaning the most

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -8,7 +8,8 @@ const CollectionPreview = ({title, items}) => (
     <div className="preview">
       { items
         .filter((item, index) => index < 4)
-        .map(item => <div key={item.id}>{item.name}</div>)}
+        .map(item => <div key={item.id}>{item.name}</div>)
+      }
     </div>
   </div>
 )
