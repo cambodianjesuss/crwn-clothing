2cc2938f635627be06bde6d4128905635132e712
Boi Soth <cambodianjesuss@gmail.com>
2022-01-01 20:55:35 -0800

    sign in component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage}/>
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/components/sign-in/sign-in.component.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {}
+  }
+
+  render(){
+    return(
+      <div>
+        Sign In Component
+      </div>
+    )
+  }
+}
+
+export default SignIn;
