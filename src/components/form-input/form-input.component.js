35e959b34fa5d8cfaeb9abcde9fb028f8a40afbd
Boi Soth <cambodianjesuss@gmail.com>
2022-01-01 22:15:15 -0800

    adding onchange to input component

--- src/components/form-input/form-input.component.js
+++ src/components/form-input/form-input.component.js
@@ -4,7 +4,7 @@ import './form-input.styles.scss';
 
 const FormInput = ({ handleChange, label, ...otherProps}) => (
   <div className='group'>
-    <input className='form-input' handleChange={handleChange} {...otherProps} />
+    <input className='form-input' onChange={handleChange} {...otherProps} />
     {
       // Render label if ther is label prop
       label ? 
