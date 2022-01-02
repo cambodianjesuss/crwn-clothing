b83b4969cdbc2e776545cd25590984187a1a8c79
Boi Soth <cambodianjesuss@gmail.com>
2022-01-01 21:05:00 -0800

    setting up sign in form

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -12,11 +12,34 @@ class SignIn extends React.Component {
     }
   }
 
+  handleSubmit = (e) => {
+    e.prventDefault();
+
+    this.setState({ email: '', password: ''})
+}
+
   render(){
     return(
       <div className="sign-in">
         <h1>I already have an accound</h1>
         <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            name="email" 
+            type="email"
+            value={this.state.email} 
+            required />
+          <label for="email">Email</label>
+          <input 
+            name="password" 
+            type="password" 
+            value={this.state.password} 
+            required />
+          <label for="password">Password</label>
+
+          <input type="submit" value="Submit Form" />
+        </form>
       </div>
     )
   }
