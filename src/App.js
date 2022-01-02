4a03e78d8e7d197ec70db5624c4d3e724a2ca1e3
Boi Soth <cambodianjesuss@gmail.com>
2022-01-02 14:32:00 -0800

    adding auth to our app

--- src/App.js
+++ src/App.js
@@ -7,19 +7,30 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
+class App extends React.Component{
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={ShopPage}/>
-        <Route path="/signin" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage}/>
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
