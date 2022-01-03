1900eb3f2df7b1580c47b780ba773b3587a4d1de
Boi Soth <cambodianjesuss@gmail.com>
2022-01-02 21:53:17 -0800

    adding signout button tereny header options

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+// Import firebase auth from util file
+import { auth } from '../../firebase/firebase.utils';
+
 // Import non react exported file 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss'
 
-const Header = () => (
+const Header = ( {currentUser} ) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo' />
@@ -13,6 +17,13 @@ const Header = () => (
     <div className='options'>
       <Link className='option' to='/shop'>SHOP</Link>
       <Link className='option' to='/contact'>CONCTACT</Link>
+      {
+        // Sign In/Sign Out if currentUser has null signed in
+        currentUser ?
+          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+        :
+          <Link className='option' to='/signin'>SIGN IN</Link>
+      }
     </div>
   </div>
 )
