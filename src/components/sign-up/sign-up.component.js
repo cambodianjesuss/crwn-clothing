4c659af1f18e538478bc6553358d1bf51c637db2
Boi Soth <cambodianjesuss@gmail.com>
2022-01-04 19:15:41 -0800

    added handleSubmit to create user with email, then we create a user profile since they will be authenticated from that point on insde of firebase.utils file

--- src/components/sign-up/sign-up.component.js
+++ src/components/sign-up/sign-up.component.js
@@ -19,6 +19,34 @@ class SignUp extends React.Component {
     };
   }
 
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert("passowrds don't match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password,
+      );
+
+      await createUserProfileDocument(user, { displayName });
+      this.setState({
+        displayName,
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   render() {
     return (
       <div className="sign-up">
