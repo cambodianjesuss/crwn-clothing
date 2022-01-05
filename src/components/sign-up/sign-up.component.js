9240956e01b5b0def19e319610ff23bb0e9210a8
Boi Soth <cambodianjesuss@gmail.com>
2022-01-04 17:59:51 -0800

    added forminput component to sign up

--- src/components/sign-up/sign-up.component.js
+++ src/components/sign-up/sign-up.component.js
@@ -11,11 +11,55 @@ class SignUp extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
   }
 
   render() {
-    return <div>Sign Up</div>;
+    return (
+      <div className="sign-up">
+        <h2 className="title">I don't have an account</h2>
+        <span>Sign up with you email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={this.handleChange}
+            label="Display Name"
+            required
+          ></FormInput>
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+          ></FormInput>
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            label="Password"
+            required
+          ></FormInput>
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label="Confirm Password"
+            required
+          ></FormInput>
+        </form>
+      </div>
+    );
   }
 }
 
