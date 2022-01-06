62631f6e66383feb29d236569509aa36ab34e130
Boi Soth <cambodianjesuss@gmail.com>
2022-01-05 22:47:29 -0800

    added our action and connected the store to mapstatetoprops in our header compoonent

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,36 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 // Import firebase auth from util file
-import { auth } from '../../firebase/firebase.utils';
+import { auth } from "../../firebase/firebase.utils";
 
-// Import non react exported file 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+// Import non react exported file
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import './header.styles.scss'
+import "./header.styles.scss";
 
-const Header = ( {currentUser} ) => (
-  <div className='header'>
-    <Link className='logo-container' to="/">
-      <Logo className='logo' />
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
     </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>SHOP</Link>
-      <Link className='option' to='/contact'>CONCTACT</Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/contact">
+        CONCTACT
+      </Link>
       {
         // Sign In/Sign Out if currentUser has null signed in
 
-        
-        currentUser ?
-          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-        :
-          <Link className='option' to='/signin'>SIGN IN</Link>
+        currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )
       }
-      {
-        console.log(currentUser)
-      }
+      {console.log(currentUser)}
     </div>
   </div>
-)
+);
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
