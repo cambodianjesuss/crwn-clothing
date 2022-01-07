97a29c1dc4b7d6361f15e6c5359ecf9782bd7aab
Boi Soth <cambodianjesuss@gmail.com>
2022-01-07 00:24:35 -0800

    more notes

---
+++ src/redux/notes.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/jsx-no-undef */
+/**
+ * Redux Notes
+ */
+
+// 1) Installing dependencies and middleware
+// npm install redux redux-logger react-redux
+
+// 2) Import the redux provider into our index.js (main app file) to give access to reducers and store
+// #index.js
+
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+
+// 2a) Provider is the component we want to wrap around our entire application
+// #index.js
+
+// eslint-disable-next-line no-undef
+ReactDOM.render(
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root"),
+);
+
+// 3) Once we wrapped our all with the Provider component, we need to create a store and pass it
+
+// 3a) Before we pass
