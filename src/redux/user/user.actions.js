84b54436917c9e999041263eba3b27d1bbfd4324
Boi Soth <cambodianjesuss@gmail.com>
2022-01-12 00:14:04 -0800

    Created a action.types file to hold the string values for action creaters and reducers from typos

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,8 +1,10 @@
+import { UserActionTypes } from "./user.types";
+
 // Action to used for dispatch in component when we need to tell the reducer to update the store
 // The store applies it on 'user'
 
 // Each action we can add any particular string to the type, so our reducer can see what the action is
 export const setCurrentUser = (user) => ({
-  type: "SET_CURRENT_USER",
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
