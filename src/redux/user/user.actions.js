c7274e47da96b134e9bea692b0c6aa1a5b5e4ef1
Boi Soth <cambodianjesuss@gmail.com>
2022-01-11 21:47:05 -0800

    base user action to dispatch in our app file

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,7 @@
 // Action to used for dispatch in component when we need to tell the reducer to update the store
 // The store applies it on 'user'
+
+// Each action we can add any particular string to the type, so our reducer can see what the action is
 export const setCurrentUser = (user) => ({
   type: "SET_CURRENT_USER",
   payload: user,
