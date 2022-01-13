13120254ddb5cdcb9d57b849428bfda14628b5d7
Boi Soth <cambodianjesuss@gmail.com>
2022-01-13 10:31:28 -0800

    notes on toggleCartHidden only runs a function to change it's boolean value

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,7 @@
 import CartActionTypes from "./cart.types";
 
+// All this action does is run the function to switch it's boolean value
+
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
   // payload is optional
