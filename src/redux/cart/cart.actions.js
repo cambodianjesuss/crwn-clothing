ca1ab08ef4250db5785709c31d6cb3cafe2f555a
Boi Soth <cambodianjesuss@gmail.com>
2022-01-15 09:08:53 -0800

    created the action for addItem that takes the item and returns it as payload

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -6,3 +6,8 @@ export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
   // payload is optional
 });
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
