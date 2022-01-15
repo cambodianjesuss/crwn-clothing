1352ae6796139cf68b22e115de9c3b02f8c2ac37
Boi Soth <cambodianjesuss@gmail.com>
2022-01-15 09:06:01 -0800

    Created an ADD_ITEM state array to add to our cartItems array -- Which we spread current state and append the action.payload

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,8 @@
 import CartActionTypes from "./cart.types";
-const INITIAL_STATE = { hidden: true };
+const INITIAL_STATE = {
+  hidden: true,
+  cartItems: [],
+};
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -7,6 +10,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden, // Opposite of what the state is -- toggles boolean value
+      };
+    }
+    case CartActionTypes.ADD_ITEM: {
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload], // Spreading all current items with new
       };
     }
     default: {
