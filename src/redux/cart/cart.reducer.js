2c8792a6452cdb7cbc9772d9769d542bb4ab9ada
Boi Soth <cambodianjesuss@gmail.com>
2022-01-16 06:36:42 -0800

    Notes for reduces based on action types

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -6,12 +6,20 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    /**
+     * TOGGLE_CART_HIDDEN
+     * Boolean which reverses it's current state to add a hidden class to shopping cart
+     */
     case CartActionTypes.TOGGLE_CART_HIDDEN: {
       return {
         ...state,
         hidden: !state.hidden, // Opposite of what the state is -- toggles boolean value
       };
     }
+    /**
+     * ADD_ITEM
+     * Spread previous state cart items with new cart items payload
+     */
     case CartActionTypes.ADD_ITEM: {
       return {
         ...state,
