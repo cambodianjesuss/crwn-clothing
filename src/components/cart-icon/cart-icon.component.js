030a72231c0d76585d9c93ea68a3f88e59657b66
Boi Soth <cambodianjesuss@gmail.com>
2022-01-18 19:25:07 -0800

    note on changeing our selector propto cache since the reducer will re-render it's state

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -18,6 +18,8 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHiddenProp: () => dispatch(toggleCartHidden()), // Attach our prop button to toggleCartHidden Action
 });
 
+// Selector prop that we should change for memoization  -- remove comment when implemented
+
 const mapStateToProps = ({ cart: { cartItems } }) => ({
   itemCount: cartItems.reduce(
     (accumilatedItems, cartItem) => accumilatedItems + cartItem.quantity,
