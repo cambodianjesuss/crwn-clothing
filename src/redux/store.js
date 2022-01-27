462b52d2708947f018f65f6a2faf4a8b3b34bd2e
Boi Soth <cambodianjesuss@gmail.com>
2022-01-26 23:42:27 -0800

    Adding redux-persist to store redux state in local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,19 @@
+/**
+ * Redux Persist
+ * Import persistStore module to store redux state in local storage
+ * We export store seperate to pass into persistStore() function
+ */
+
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const persistor = persistStore(store);
 
 export default store;
